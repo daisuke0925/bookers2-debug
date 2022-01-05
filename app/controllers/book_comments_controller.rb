9d1777ed9fb791da9f9e99939cdc9d696c0405bb
daisuke0925 <93876241+daisuke0925@users.noreply.github.com>
2022-01-05 08:10:01 +0000

    [Finish]いいね、コメント機能-2

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,11 +1,17 @@
 class BookCommentsController < ApplicationController
 
   def create
-    book = Book.find(params[:book_id])
+    @book = Book.find(params[:book_id])
     comment = current_user.book_comments.new(book_comment_params)
-    comment.book_id = book.id
-    comment.save
-    redirect_to book_path(book)
+    comment.book_id = @book.id
+    if comment.save
+        redirect_to book_path(@book)
+    else
+        @error_comment = comment
+        @user = @book.user
+        @book_comment = BookComment.new
+        render 'books/show'
+    end
   end
 
   def destroy
