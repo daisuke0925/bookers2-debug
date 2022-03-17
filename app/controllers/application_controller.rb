ba7a6f28217b6dd7fbf7129198e83695dea24dcd
daisuke0925 <93876241+daisuke0925@users.noreply.github.com>
2022-03-17 09:08:41 +0000

    アプリケーションコントローラー修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,9 +5,9 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
-  def after_sign_in_path_for(resource)
-    admin_items_path
-  end
+  # def after_sign_in_path_for(resource)
+  #   admin_items_path
+  # end
 
   def after_sign_out_path_for(resource)
     new_admin_session
